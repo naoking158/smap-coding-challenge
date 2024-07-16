49cd8238706d16f24a0000a9e90b6a4f897bff46
naoking158 <29372455+naoking158@users.noreply.github.com>
2024-07-16 12:23:17 +0900

    feat: detail ページの URL を追加

--- dashboard/consumption/urls.py
+++ dashboard/consumption/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     re_path(r'^$', views.summary),
     re_path(r'^summary/', views.summary),
-    re_path(r'^detail/', views.detail),
+    re_path(r'^detail/(?P<user_id>\d+)/$', views.detail, name='detail'),
 ]
