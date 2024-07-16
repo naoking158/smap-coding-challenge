8b4db383b7da7180b933e4a940aafdee9a820100
naoking158 <29372455+naoking158@users.noreply.github.com>
2024-07-16 11:10:17 +0900

    style: ruff のリントとフォーマットを適用
    実行コード
    ```sh
    $ ruff check --fix; ruff format
    ```

--- dashboard/dashboard/urls.py
+++ dashboard/dashboard/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import re_path, include
+
 from django.contrib import admin
+from django.urls import include, re_path
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
-    re_path(r'^', include('consumption.urls'))
+    re_path(r'^', include('consumption.urls')),
 ]
