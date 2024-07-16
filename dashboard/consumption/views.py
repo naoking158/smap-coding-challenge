f984b3639806604778816bbed743e6d6a75a13bb
naoking158 <29372455+naoking158@users.noreply.github.com>
2024-07-16 12:22:43 +0900

    feat: summary ページを更新

--- dashboard/consumption/views.py
+++ dashboard/consumption/views.py
@@ -3,13 +3,19 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+from consumption.chart.generate import (
+    generate_daily_total_consumption_graph,
+    generate_daily_total_consumption_graph_by_area,
+)
+from consumption.models import User
 
 
 def summary(request):
-    context = {
-        'message': 'Hello!',
-    }
+    graph = generate_daily_total_consumption_graph()
+    graph_by_area = generate_daily_total_consumption_graph_by_area()
+    user_ids = list(User.objects.values_list('id', flat=True).order_by('id'))
+
+    context = {'graph': graph, 'graph_by_area': graph_by_area, 'user_ids': user_ids}
     return render(request, 'consumption/summary.html', context)
 
 
