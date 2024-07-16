fa6f17b971a7319b79b026f1a5e5eeb46f57d06b
naoking158 <29372455+naoking158@users.noreply.github.com>
2024-07-16 12:09:15 +0900

    chore: モデルを admin ページに登録

--- dashboard/consumption/admin.py
+++ dashboard/consumption/admin.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from consumption.models import Consumption, User
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Consumption)
